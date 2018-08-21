66b76581f7dafc8af01ba8ab625e2dcc300e4956
U6041818 <U6041818@U6041818-TPL-A>
2018-08-21 13:14:20 -0500

    Minor change

--- PillCards/PatientManager.cpp
+++ PillCards/PatientManager.cpp
@@ -4,7 +4,7 @@ void PatientManager::AddNewPatient(Patient&& p)
 {
 	this->m_listOfPatients.emplace(p.GetId(), std::make_unique<Patient>(std::forward<Patient>(p)));
 	//TODO: Might need to make the PillCard object a shared pointer
-	//instead of a unique pointer so that the patient objection doesn't lose ownership
+	//instead of a unique pointer so that the patient object doesn't lose ownership
 }
 
 const Patient& PatientManager::GetPatient(uint64_t patientId) const
